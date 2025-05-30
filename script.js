41d0e05e8da59757e56ab48c58d246a01f16687f
tik1tok <jardeinsharon57@gmail.com>
2025-05-30 20:42:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ if (agent.indexOf('music') >= 0) {
   gif.className = 'block';
   gif_desc.className = 'block';
 } else {
-  window.location = 'https://onlyfans.com/skinnycurvyalexa/c484';
-//   window.open('https://onlyfans.com/skinnycurvyalexa/c484', '_blank');
+  window.location = 'https://onlyfans.com/alicruzvip/c33';
+//   window.open('https://onlyfans.com/alicruzvip/c33', '_blank');
   console.log('go to link');
 
   wrap.className = 'block';
